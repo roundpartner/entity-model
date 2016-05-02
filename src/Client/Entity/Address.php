8ace71c12f0e18862354a08470bc06ac7abcb83c
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2016-05-02 14:28:00 +0100

    Create address

---
+++ src/Client/Entity/Address.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace RoundPartner\Model\Client\Entity;
+
+/**
+ * @property integer $address_id
+ * @property integer $customer_id
+ * @property string $house
+ * @property string $address_1
+ * @property string $address_2
+ * @property string $town
+ * @property string $county
+ * @property string $postcode
+ * @property string $address_status
+ * @property string $latitude
+ * @property string $longitude
+ * @property integer $invoice_default
+ * @property string $note_id
+ * @property string $modified
+ * @property string $created
+ */
+class Address
+{
+
+}
--- src/Client/Entity/Customer.php
+++ src/Client/Entity/Customer.php
@@ -3,7 +3,7 @@
 namespace RoundPartner\Model\Client\Entity;
 
 /**
- * @property string $customer_id
+ * @property integer $customer_id
  * @property string $business_name
  * @property string $title
  * @property string $first_name
@@ -11,7 +11,7 @@ namespace RoundPartner\Model\Client\Entity;
  * @property string $telephone
  * @property string $mobile
  * @property string $email
- * @property string $note_id
+ * @property integer $note_id
  * @property string $customer_status
  * @property string $created
  * @property string $modified
